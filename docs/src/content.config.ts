5c6283f510a75ba01ed91a2b33543fc629361476
Felix Andreas <felix.andreas95@googlemail.com>
2025-03-04 22:50:20 +0100

    docs: Simplify directory structure

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,12 +1,15 @@
 import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
+import { glob } from 'astro/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { ExtendDocsSchema } from 'starlight-theme-black/schema'
 
 
 export const collections = {
 	docs: defineCollection({
-		loader: docsLoader(),
+		loader: glob({
+			base: "./content",
+			pattern: `**/[^_]*.{${["md", "mdx"].join(',')}}`,
+		}),
 		schema: docsSchema({
 			extend: ExtendDocsSchema,
 		}),
