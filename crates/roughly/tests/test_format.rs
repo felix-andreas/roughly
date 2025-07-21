492fc1aac115219a9a4837a2bb71ee860812b445
Felix Andreas <felix.andreas95@googlemail.com>
2025-07-22 00:17:06 +0200

    update

--- crates/roughly/tests/test_format.rs
+++ crates/roughly/tests/test_format.rs
@@ -13,17 +13,17 @@ fn base() {
     run_test_groups(&parse_test_file(BASE_TESTS));
 }
 
-// #[test]
-// fn special() {
-//     const SPECIAL_TESTS: &str = include_str!("format/special.R.test");
-//     run_test_groups(&parse_test_file(SPECIAL_TESTS));
-// }
+#[test]
+fn special() {
+    const SPECIAL_TESTS: &str = include_str!("format/special.R.test");
+    run_test_groups(&parse_test_file(SPECIAL_TESTS));
+}
 
-// #[test]
-// fn misc() {
-//     const MISC_TESTS: &str = include_str!("format/misc.R.test");
-//     run_test_groups(&parse_test_file(MISC_TESTS));
-// }
+#[test]
+fn misc() {
+    const MISC_TESTS: &str = include_str!("format/misc.R.test");
+    run_test_groups(&parse_test_file(MISC_TESTS));
+}
 
 #[test]
 fn trailing_spaces_in_comments() {
